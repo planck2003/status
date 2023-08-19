d85b9a587dedd7707c023721a9dbc7a46232682b
planck2003 <139834951+planck2003@users.noreply.github.com>
2023-08-19 11:22:31 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,33 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: '新奥尔良姬翅 站点检测',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'u2263932-ce171fdc9ef57bc4870a8a32',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: '主页',
+      url: 'https://status.yejinblok.cn/'
+    },
+    {
+      text: 'GitHub',
+      url: 'https://github.com/planck2003'
+    },
+    {
+      text: '博客',
+      url: 'https://www.yejinblok.cn'
+    },
+  ],
+};
